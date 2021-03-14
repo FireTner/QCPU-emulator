5eb7fac9c3e73de2622c8deae8f1e2191e9dae35
DPaul-sys <DPaul2143@gmail.com>
2021-03-14 12:21:05 +0100

    asthetics

--- qcpu/qcpu-bin.h
+++ qcpu/qcpu-bin.h
@@ -7,7 +7,7 @@
 
 uint8_t* QCPU_makeStorage(const unsigned int size)
 {
-    return (uint8_t*) malloc(size);
+    return (uint8_t*)malloc(size);
 }
 
 void QCPU_loadProgram(uint8_t* memory, const char* filePath)
